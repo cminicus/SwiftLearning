8c9ad1cc086e4dbfa093eebae33cf7e33f8045b8
Clayton Minicus <cminicu1@jhu.edu>
2015-10-15 10:18:56 -0400

    Initial Commit

---
+++ SwiftLearning/SwiftLearning.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLearning.h
+//  SwiftLearning
+//
+//  Created by Clayton Minicus on 10/15/15.
+//  Copyright © 2015 Clayton Minicus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLearning.
+FOUNDATION_EXPORT double SwiftLearningVersionNumber;
+
+//! Project version string for SwiftLearning.
+FOUNDATION_EXPORT const unsigned char SwiftLearningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLearning/PublicHeader.h>
+
+
